65e4d174405c557ac85ad48173c4d0fb5c9f32b0
rick0376 <seu-email@dominio.com>
2025-06-12 11:06:49 -0300

    Projeto Finalizado - aula deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionAuthProvider } from '@/components/session-auth'
+import { Toaster } from 'sonner'
+import { QueryClientContext } from '@/providers/queryclient'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionAuthProvider>
+          <QueryClientContext>
+            <Toaster
+              duration={2500}
+            />
+            {children}
+          </QueryClientContext>
+        </SessionAuthProvider>
       </body>
     </html>
   );
