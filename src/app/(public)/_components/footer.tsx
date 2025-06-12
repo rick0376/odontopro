abf28f69ed814b9cadbf449ae85e0d00e5405979
rick0376 <seu-email@dominio.com>
2025-06-12 16:06:39 -0300

    atualizando foother

--- src/app/(public)/_components/footer.tsx
+++ src/app/(public)/_components/footer.tsx
@@ -3,7 +3,9 @@ export function Footer() {
     <footer className="py-6 text-center text-gray-500 text-sm md:text-base">
       <p>
         Todos direitos reservados © {new Date().getFullYear()} -{" "}
-        <span className="hover:text-black duration-300">@rickpereira</span>
+        <span className="hover:text-black duration-300">
+          @LHPSYSTEMS - rickpereira
+        </span>
       </p>
     </footer>
   );
